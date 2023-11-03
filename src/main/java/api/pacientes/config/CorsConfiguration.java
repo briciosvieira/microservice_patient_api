8c1f2b491a8ad6bad32445bd1e73282c745c7d3a
Gabriel <gabriel.souza@saude.ba.gov.br>
2023-11-02 22:00:52 -0300

    corsConfiguration

---
+++ src/main/java/api/pacientes/config/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package api.pacientes.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8083")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
