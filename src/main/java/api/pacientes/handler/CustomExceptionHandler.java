d80a4f00cb3fa8b0a60f0394399f31d8c10153e4
Ivan Júnior <ivan.junior2706@gmail.com>
2023-11-23 23:09:59 -0300

    Fix: invalid date format exception.

--- src/main/java/api/pacientes/handler/CustomExceptionHandler.java
+++ src/main/java/api/pacientes/handler/CustomExceptionHandler.java
@@ -6,11 +6,14 @@ import api.pacientes.handler.exceptions.ResourceNotFoundException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.format.DateTimeParseException;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @ControllerAdvice
@@ -48,9 +51,37 @@ public class CustomExceptionHandler {
         return new ResponseEntity<>(body, HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
+    @ExceptionHandler(RegisterBadRequestException.class)
+    public ResponseEntity<Object> handleRegisterBadRequest(RegisterBadRequestException ex) {
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("mensagem", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        Throwable rootCause = ex.getRootCause();
+        if (rootCause instanceof DateTimeParseException) {
+            Map<String, Object> body = new LinkedHashMap<>();
+            body.put("mensagem", "Formato de data inválido. Use o formato 'yyyy-MM-dd'");
+
+            return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleAllExceptions(Exception ex) {
-        if (ex instanceof MethodArgumentNotValidException || ex instanceof ResourceNotFoundException || ex instanceof DuplicateKeyException) {
+        if (
+            ex instanceof MethodArgumentNotValidException ||
+            ex instanceof ResourceNotFoundException ||
+            ex instanceof DuplicateKeyException ||
+            ex instanceof RegisterBadRequestException ||
+            ex instanceof HttpMessageNotReadableException
+        ) {
             return null;
         }
 
@@ -60,14 +91,5 @@ public class CustomExceptionHandler {
                 " contato pelo nosso WhatsApp 71 99999-9999. Lamentamos o ocorrido!");
 
         return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    @ExceptionHandler(RegisterBadRequestException.class)
-    public ResponseEntity<Object> handleRegisterBadRequest(RegisterBadRequestException ex) {
-        Map<String, Object> body = new HashMap<>();
-
-        body.put("mensagem", ex.getMessage());
-
-        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 }
